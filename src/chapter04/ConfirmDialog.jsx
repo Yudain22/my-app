d8b78ecf1242e8ddf3e0d97b18528598fa571644
Yudain22 <di4046@gmail.com>
2024-05-16 11:32:37 +0900

    컴포넌트 합성

--- src/chapter04/ConfirmDialog.jsx
+++ src/chapter04/ConfirmDialog.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-function Button(props){
-    return(
+function Button(props) {
+    return (
         <button className={`bg-${props.color}`}>
             <b>
                 {props.children}
@@ -10,8 +10,8 @@ function Button(props){
     );
 }
 
-function ConfirmDialog(props){
-    return(
+function ConfirmDialog(props) {
+    return (
         <div>
             <p>내용을 확인하셨으면 확인 버튼을 눌러주세요.</p>
             <Button color='green'>확인</Button>
--- src/index.js
+++ src/index.js
@@ -6,30 +6,32 @@ import reportWebVitals from './reportWebVitals';
 // import StudentList from './chapter03/StudentList';
 // import Library from './chapter03/Library';
 // import ConfirmDialog from './chapter04/ConfirmDialog';
-import Clock from './chapter04/Clock';
+// import Clock from './chapter04/Clock';
+import PropsEx from './chapter05/PropsEx';
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-// root.render(
-//   <React.StrictMode> 
-//       {/* <App /> */} 
-//       {/* <Library />  */}
-//       {/* <StudentList />  */}
-//   {/* <ConfirmDialog/> */}
-//   </React.StrictMode>
-// );
+root.render(
+  <React.StrictMode> 
+      {/* <App /> */} 
+      {/* <Library />  */}
+      {/* <StudentList />  */}
+  {/* <ConfirmDialog/> */}
+  <PropsEx/>
+  </React.StrictMode>
+);
 
-setInterval(()=>{
-  root.render( 
-    <React.StrictMode> 
+// setInterval(()=>{
+//   root.render( 
+//     <React.StrictMode> 
     
-  <Clock/>
-      </React.StrictMode>
-  );
-//밀리세컨이라서 1000이 1초
-},1000)
+//   <Clock/>
+//       </React.StrictMode>
+//   );
+// //밀리세컨이라서 1000이 1초
+// },1000)
 
 
 // If you want to start measuring performance in your app, pass a function
