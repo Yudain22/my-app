c39334ec370f98c040605ed2a8998d7e120629d3
Yudain22 <di4046@gmail.com>
2024-05-16 09:52:17 +0900

    엘리먼트 렌더링

--- src/index.js
+++ src/index.js
@@ -4,15 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 // import StudentList from './chapter03/StudentList';
-import Library from './chapter03/Library';
+// import Library from './chapter03/Library';
+import ConfirmDialog from './chapter04/ConfirmDialog';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render( 
   <React.StrictMode> 
     {/* <App /> */} 
-    <Library /> 
-    {/* <StudentList /> */} 
+    {/* <Library />  */}
+    {/* <StudentList />  */}
+<ConfirmDialog/>
     </React.StrictMode>
 );
 
