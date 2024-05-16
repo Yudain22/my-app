bc547e6c897ec8f9ab7ffd57bc0e505449544273
Yudain22 <di4046@gmail.com>
2024-05-16 12:26:00 +0900

    컴포넌트 데이터를 별도의 객체로 분리하기

--- src/chapter05/Comment.jsx
+++ src/chapter05/Comment.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 
 function Comment(props) {
-    return(
+    return (
         <div style={styles.wrapper}>
             <div style={styles.imageContainer}>
-            <img
-            src="https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png"
-            style={styles.image}/>
+                <img
+                    src="https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png"
+                    style={styles.image} 
+                />
             </div>
 
             <div style={styles.contentContainer}>
                 <span style={styles.nameText}>{props.name}</span>
-                <span style={styles.commentText}>{props.Comment}
+                <span style={styles.commentText}>{props.comment}
                 </span>
             </div>
         </div>
@@ -19,34 +20,34 @@ function Comment(props) {
 }
 
 const styles = {
-    wrapper:{
+    wrapper: {
         margin: 8,
         padding: 8,
         display: "flex",
-        flexDirection:"row",
-        border:"1px solid grey",
+        flexDirection: "row",
+        border: "1px solid grey",
         borderFadius: 16,
     },
-    imageContainer:{},
-    image:{
+    imageContainer: {},
+    image: {
         width: 50,
-        height:50,
-        borderRadius:25,
+        height: 50,
+        borderRadius: 25,
     },
-    contentContainer:{
-        marginLeft:8,
-        display:"flex",
-        flexDirection:"column",
-        justifyContent:"center",
+    contentContainer: {
+        marginLeft: 8,
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
     },
     nameText: {
-        color:"black",
-        fontSize:16,
-        fontWeight:"bold",
+        color: "black",
+        fontSize: 16,
+        fontWeight: "bold",
     },
-    commentText:{
-        color:"black",
-        fontSize:16,
+    commentText: {
+        color: "black",
+        fontSize: 16,
     },
 };
 
